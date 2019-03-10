b9652b63b7aa5d6f348d74d82bba67230161e433
teodoratimcu <teodoratimcu@gmail.com>
2019-03-10 23:46:19 +0200

    selinux

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,10 @@ echo "bootstrap.sh is running"
 
 echo "Updating system software"
 yum -y update
+
+
+if grep -q SELINUX="disabled" /etc/selinux/config; then
+	echo "SELINUX is disabled"
+fi
+
+setenforce 0
--- config.py
+++ config.py
@@ -18,15 +18,13 @@ for c in commands:
     print "Running command: " + c
     subprocess.call(c, shell=True)
 
-if not os.path.exists("tema1_MLMOS"):
-    print "Cloning git repository"
-    subprocess.call("git clone https://github.com/teodoratimcu/tema1_MLMOS.git", shell=True)
-    os.chdir("tema1_MLMOS")
-else:
-    print "Changing directory to tema1_MLMOS"
-    os.chdir("tema1_MLMOS")
-    print "Pulling git repository"
-    subprocess.call("git pull", shell=True)
+if os.path.exists("tema1_MLMOS"):
+    print "Removing tema1_MLMOS"
+    subprocess.call("rm -rf tema1_MLMOS", shell=True)
+
+print "Cloning git repository"
+subprocess.call("git clone https://github.com/teodoratimcu/tema1_MLMOS.git", shell=True)
+os.chdir("tema1_MLMOS")
 
 print "Setting permissions for bootstrap.sh"
 subprocess.call("chmod 777 bootstrap.sh", shell=True)
